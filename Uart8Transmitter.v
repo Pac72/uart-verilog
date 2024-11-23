488119864f467f23ea396da1b654f9f13e44cd99
Paolo Cancedda <paolo.cancedda@gmail.com>
2024-11-23 14:51:36 +0100

    Better use a full reset when en == 0

--- Uart8Transmitter.v
+++ Uart8Transmitter.v
@@ -49,90 +49,86 @@ module Uart8Transmitter #(
     output reg out       // tx line for serial data
 );
 
-reg [2:0] state     = `RESET;
-reg [7:0] in_data   = 8'b0; // shift reg for the data to transmit serially
-reg [2:0] bit_index = 3'b0; // index for 8-bit data
-
-/*
- * Disable at any time in the flow
- */
-always @(posedge clk) begin
-    if (!en) begin
-        state <= `RESET;
-    end
-end
+reg [2:0] state;
+reg [7:0] in_data; // shift reg for the data to transmit serially
+reg [2:0] bit_index; // index for 8-bit data
 
 /*
  * State machine
  */
 always @(posedge clk) begin
-    case (state)
-        `RESET: begin
-            // state variables
-            bit_index <= 3'b0;
-            // outputs
-            busy      <= 1'b0;
-            done      <= 1'b0;
-            out       <= 1'b1; // drive the line high for IDLE state
-            // next state
-            if (en) begin
+    if (en) begin
+        case (state)
+            `RESET: begin
+                // state variables
+                bit_index <= 3'b0;
+                // outputs
+                busy      <= 1'b0;
+                done      <= 1'b0;
+                out       <= 1'b1; // drive the line high for IDLE state
+                // next state
                 state <= `IDLE;
             end
-        end
 
-        `IDLE: begin
-            if (start) begin
-                in_data <= in; // register the input data
-                state   <= `START_BIT;
-            end
-        end
-
-        `START_BIT: begin
-            bit_index <= 3'b0;
-            busy      <= 1'b1;
-            done      <= 1'b0;
-            out       <= 1'b0; // send the space output, aka start bit (low)
-            state     <= `DATA_BITS;
-        end
-
-        `DATA_BITS: begin // take 8 clock cycles for data bits to be sent
-            // grab each input bit using a shift register: the hardware
-            // realization is simple compared to routing the access
-            // dynamically, i.e. using in_data[bit_index]
-            in_data   <= { 1'b0, in_data[7:1] };
-            out       <= in_data[0];
-            // manage the state transition
-            bit_index <= bit_index + 3'b1;
-            if (&bit_index) begin
-                // bit_index wraps around to zero
-                state <= `STOP_BIT;
-            end
-        end
-
-        `STOP_BIT: begin
-            done              <= 1'b1; // signal the transmission stop
-            out               <= 1'b1; // transition to mark state output (high)
-            if (start) begin
-                if (done == 1'b0) begin // this distinguishes 2 sub-states
-                    in_data   <= in; // register new input data
-                    if (TURBO_FRAMES) begin
-                        state <= `START_BIT; // go direct to transmit
-                    end else begin
-                        state <= `STOP_BIT; // keep mark state one extra cycle
-                    end
-                end else begin // there was extra cycle within this state
-                    done      <= 1'b0;
-                    state     <= `START_BIT; // now go to transmit
+            `IDLE: begin
+                if (start) begin
+                    in_data <= in; // register the input data
+                    state   <= `START_BIT;
                 end
-            end else begin
-                state         <= `RESET;
             end
-        end
 
-        default: begin
-            state <= `RESET;
-        end
-    endcase
+            `START_BIT: begin
+                bit_index <= 3'b0;
+                busy      <= 1'b1;
+                done      <= 1'b0;
+                out       <= 1'b0; // send the space output, aka start bit (low)
+                state     <= `DATA_BITS;
+            end
+
+            `DATA_BITS: begin // take 8 clock cycles for data bits to be sent
+                // grab each input bit using a shift register: the hardware
+                // realization is simple compared to routing the access
+                // dynamically, i.e. using in_data[bit_index]
+                in_data   <= { 1'b0, in_data[7:1] };
+                out       <= in_data[0];
+                // manage the state transition
+                bit_index <= bit_index + 3'b1;
+                if (&bit_index) begin
+                    // bit_index wraps around to zero
+                    state <= `STOP_BIT;
+                end
+            end
+
+            `STOP_BIT: begin
+                done              <= 1'b1; // signal the transmission stop
+                out               <= 1'b1; // transition to mark state output (high)
+                if (start) begin
+                    if (done == 1'b0) begin // this distinguishes 2 sub-states
+                        in_data   <= in; // register new input data
+                        if (TURBO_FRAMES) begin
+                            state <= `START_BIT; // go direct to transmit
+                        end else begin
+                            state <= `STOP_BIT; // keep mark state one extra cycle
+                        end
+                    end else begin // there was extra cycle within this state
+                        done      <= 1'b0;
+                        state     <= `START_BIT; // now go to transmit
+                    end
+                end else begin
+                    state         <= `RESET;
+                end
+            end
+
+            default: begin
+                state <= `RESET;
+            end
+        endcase
+    end
+    else begin
+        state <= `RESET;
+        in_data <= 8'b0;
+        bit_index <= 3'b0;
+    end
 end
 
 endmodule
